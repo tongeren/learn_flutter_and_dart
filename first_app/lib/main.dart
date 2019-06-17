a28d0cb73c22a133f1450c7c8a88c6255e510755
Duncan van Tongeren <dpvantongeren@gmail.com>
2019-06-17 18:55:25 +0700

    Rewriting with a stateful widget.

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,18 +24,22 @@ class MyApp extends StatelessWidget {
             Container(
               margin: EdgeInsets.all(10.0),
               child: RaisedButton(
-                onPressed: () {}, 
+                onPressed: () {
+
+                }, 
                 child: Text('Add Product'),
               ),
             ),
-            Card(
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assets/food.jpg'),
-                  Text('Food Paradise')
-                ],
-              ),
-            ),
+            Column(children: _products
+              .map((element) => Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assets/food.jpg'),
+                    Text(element)
+                  ],
+                ),
+              )).toList()
+            ), 
           ],
         ),  
       ),
