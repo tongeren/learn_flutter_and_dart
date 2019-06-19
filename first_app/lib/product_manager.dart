491e03a66e4cb486d858b6396fe3e555495b13b0
Duncan van Tongeren <dpvantongeren@gmail.com>
2019-06-19 13:39:39 +0700

    Adding a ProductControl class and lifting state up.

--- first_app/lib/product_manager.dart
+++ first_app/lib/product_manager.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './products.dart'; 
+import './product_control.dart';
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
@@ -32,23 +33,23 @@ class _ProductManagerState extends State<ProductManager> {
     super.didUpdateWidget(oldWidget);
   }
 
+  void _addProduct(String product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     print('[ProductManager Widget] build()');
-    return Column(children: [
-      Container(
-        margin: EdgeInsets.all(10.0),
-        child: RaisedButton(
-          color: Theme.of(context).primaryColor,
-          onPressed: () {
-            setState(() {
-              _products.add('Advanced Food Tester');
-            });
-          },
-          child: Text('Add Product'),
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.all(10.0),
+          child: ProductControl(_addProduct),
         ),
-      ),
-      Products(_products)
-    ]);
+        Products(_products)
+      ],  
+    );
   }
 }
