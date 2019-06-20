9796ab97290143ed5cb50c33931f6675c83a6eb6
Duncan van Tongeren <dpvantongeren@gmail.com>
2019-06-20 13:38:46 +0700

    Using the ListView and Expanded widgets.

--- first_app/lib/products.dart
+++ first_app/lib/products.dart
@@ -10,7 +10,7 @@ class Products extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print('[Products Widget] Build');
-    return Column(
+    return ListView(
       children: products
         .map(
           (element) => Card(
