5ec989ca94ee672c58f43f27391e149c1b3a4061
Duncan van Tongeren <dpvantongeren@gmail.com>
2019-06-20 16:51:23 +0700

    Refactoring the building of the product list in a new method.

--- first_app/lib/products.dart
+++ first_app/lib/products.dart
@@ -18,14 +18,25 @@ class Products extends StatelessWidget {
     );
   }
 
+  Widget _buildProductList() {
+    Widget productCard;
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      ); 
+    } else {
+      productCard = Center( 
+        child: Text('No products found. Please add some.'),
+      );
+    }
+
+    return productCard;
+  }
+
   @override
   Widget build(BuildContext context) {
     print('[Products Widget] Build');
-    return products.length > 0  ? ListView.builder(
-        itemBuilder: _buildProductItem,
-        itemCount: products.length,
-      ) : Center(
-        child: Text('No products found. Please add some.'),
-      ); 
+    return _buildProductList();
   }
 }
