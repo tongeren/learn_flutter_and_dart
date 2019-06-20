ffd5face52ada5e13a11d42a137e56a2fd1d6c57
Duncan van Tongeren <dpvantongeren@gmail.com>
2019-06-20 18:08:35 +0700

    Styling the product page.

--- first_app/lib/pages/product.dart
+++ first_app/lib/pages/product.dart
@@ -8,11 +8,20 @@ class ProductPage extends StatelessWidget {
         title: Text('Product Details'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Text('Details'),
-          RaisedButton(
-            child: Text('BACK'),
-            onPressed: () => Navigator.pop(context),
+          Image.asset('assets/food.jpg'),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Details'),
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              color: Theme.of(context).accentColor,
+              child: Text('BACK'),
+              onPressed: () => Navigator.pop(context),
+            ),
           )
         ],
       ),
