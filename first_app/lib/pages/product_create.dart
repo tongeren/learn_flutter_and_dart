009c94134808189533238485a9ff5ee6b70ad41a
Duncan van Tongeren <dpvantongeren@gmail.com>
2019-07-09 12:09:46 +0700

    Naming the private variables in _productCreatePageState more explicitly.

--- first_app/lib/pages/product_create.dart
+++ first_app/lib/pages/product_create.dart
@@ -12,9 +12,9 @@ class ProductCreatePage extends StatefulWidget {
 }
 
 class _ProductCreatePageState extends State<ProductCreatePage> {
-  String titleValue = '';
-  String descriptionValue = '';
-  double priceValue = 0.0;
+  String _titleValue = '';
+  String _descriptionValue = '';
+  double _priceValue = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +26,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
               decoration: InputDecoration(labelText: 'Product Title'),
               onChanged: (String value) {
                 setState(() {
-                  titleValue = value;
+                  _titleValue = value;
                 });
               }),
           TextField(
@@ -35,7 +35,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
               maxLines: 4,
               onChanged: (String value) {
                 setState(() {
-                  descriptionValue = value;
+                  _descriptionValue = value;
                 });
               }),
           TextField(
@@ -43,7 +43,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
               keyboardType: TextInputType.numberWithOptions(),
               onChanged: (String value) {
                 setState(() {
-                  priceValue = double.parse(value);
+                  _priceValue = double.parse(value);
                 });
               }),
           SizedBox(height: 10.0),
@@ -53,9 +53,9 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
               textColor: Colors.white,
               onPressed: () {
                 final Map<String, dynamic> product = {
-                  'title': titleValue,
-                  'description': descriptionValue,
-                  'price': priceValue,
+                  'title': _titleValue,
+                  'description': _descriptionValue,
+                  'price': _priceValue,
                   'image': 'assets/food.jpg'
                 };
                 widget.addProduct(product);
